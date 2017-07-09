eb12d166219fac0aee6177e2c44dd78b5867da60
Arun Madisetty <arun.madisetty@gmail.com>
2017-07-09 11:55:38 -0500

    Added individual links to each product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,12 @@
 class ProductsController < ApplicationController
-  def products_method
-    @products = Contact.all
-    render "products.html.erb"
+  def index
+    @products = Product.all
+    render "index.html.erb"
+  end
+
+  def show
+    product_id = params[:id]
+    @product = Product.find_by(id: product_id)
+    render "show.html.erb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/mini_capstone" => "products#products_method"
+  get "/knicksgear" => "products#index"
+  get "/knicksgear/:id" => "products#show"
 end
