50f32d95604042cf3b00eaa9f6f19ebce6033291
Arun Madisetty <arun.madisetty@gmail.com>
2017-07-12 20:15:30 -0500

    Precautionary

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -42,4 +42,10 @@ class ProductsController < ApplicationController
     render "update.html.erb"
   end
 
+  def destroy
+    product_id = params[:id]
+    @product = Product.find_by(id: product_id)
+    @product.destroy
+    render "destroy.html.erb"
+  end
 end
